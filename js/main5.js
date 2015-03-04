ecb4409082bc1426d263661d339094b722a9d9c5
ekosinski <ekosinsk@masonlive.gmu.edu>
2015-03-04 18:37:00 -0500

    Added everything

--- js/main5.js
+++ js/main5.js
@@ -1,124 +1,102 @@
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    "use strict";
-    
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
-    function preload() {
-        // Load an image and call it 'logo'.
-        //game.load.image( 'b', 'assets/phaser.png' );
-        game.load.image( 'background', 'Assets/fire_ice.png' );
-        //game.load.audio( 'bodyDry', 'assets/01 -Before my body is dry.mp3');
-        //game.load.audio('mario', 'assets/Mario_Slide.wav');
-        /*
-        game.load.image('girrafeHead','assets/girrafe_head.png');
-        game.load.image('girrafeBody','assets/girrafe_body.jpg');
-        game.load.image('girrafeLeftLeg','assets/girrafe_left_leg.png');
-        game.load.image('girrafeRightLeg','assets/girrafe_right.png');
-        game.load.image('gorillaHead','assets/gorilla_head.jpg');
-        game.load.image('gorillaLeftArm','assets/gorilla_left_arm.jpg');
-        game.load.image('gorillaRightArm','assets/gorilla_right_arm.jpg');
-        game.load.image('gorillaLeftLeg','assets/gorilla_left_leg.jpg');
-        game.load.image('gorillaRightLeg','assets/gorilla_right_leg.jpg');
-        game.load.image('gorillaBody','assets/gorilla_body.jpg');
-        game.load.image('humanHead','assets/human_head.jpg');
-        game.load.image('humanRightArm','assets/right_arm.jpg');
-        game.load.image('humanLeftArm','assets/left_arm.jpg');
-        game.load.image('humanRightLeg','assets/right_leg');
-        game.load.image('humanLeftLeg','assets/left_leg');
-        game.load.image('catHead','assets/cat_head');*/
-    }
-    
-    var background1;
-    /*var background2;
+        window.onload = function () {
+            // You might want to start with a template that uses GameStates:
+            //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
 
-    var girrafeHead;
-    var girrafeBody;
-    var girrafeLeftLeg;
-    var girrafeRightLeg;
+            // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+            // You will need to change the fourth parameter to "new Phaser.Game()" from
+            // 'phaser-example' to 'game', which is the id of the HTML element where we
+            // want the game to go.
+            // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+            // You will need to change the paths you pass to "game.load.image()" or any other
+            // loading functions to reflect where you are putting the assets.
+            // All loading functions will typically all be found inside "preload()".
 
-    var gorillaHead;
-    var gorillaLeftArm;
-    var gorillaRightArm;
-    var gorillaBody;
-    var gorillaLeftLeg;
-    var gorillaRightLeg;
+            "use strict";
 
-    var humanHead;
-    var humanLeftArm;
-    var humanRightArm;
-    var humanLeftLeg;
-    var humanRightLeg;
+            var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
-    var catHead;
+            function preload() {
+                game.load.image('roadway', 'assets/TwoLane 1024.png');
+                game.load.image('sportscar', 'assets/Car.png');
+                game.load.image('dumptruck', 'assets/DumpTruck.png')
+            }
 
-    var counter = 0;
-    var cursors;*/
+            var mycar;
+            var dumptruck;
+            var roadway;
+            var cursors;
+            var myswitch = false;
+
+            function create() {
+
+                game.physics.startSystem(Phaser.Physics.ARCADE);
+
+                // The scrolling roadway background
+                roadway = game.add.tileSprite(0, -212, 1024, 1024, 'roadway');
+
+                //  We're going to be using physics, so enable the Arcade Physics system
+                
+                //Create the images here
+                mycar = game.add.sprite(game.world.centerX - 100, game.world.centerY + 50, 'sportscar');
+                game.physics.enable(mycar, Phaser.Physics.ARCADE);
+                mycar.height = 50;
+                mycar.width = 100;
+                mycar.body.velocity.x = 0;
+
+                dumptruck = game.add.sprite(game.world.centerX+400, game.world.centerY - 125, 'dumptruck');
+                game.physics.enable(dumptruck, Phaser.Physics.ARCADE);
+                dumptruck.height = 75;
+                dumptruck.width = 150;
+                dumptruck.body.velocity.x = -200;
+
+                //  And some controls to play the game with
+                cursors = game.input.keyboard.createCursorKeys();
 
 
-function create() {
+                
+            }
 
-    /*game.physics.startSystem(Phaser.Physics.ARCADE);
+            function update() {
 
-    //  Enable the QuadTree
-    game.physics.arcade.skipQuadTree = false;
+                // Scroll the background
+                roadway.tilePosition.x -= 2;
+                
+                //  If the sprite is > 8px away from the pointer then let's move to it
+                if (game.physics.arcade.distanceToPointer(mycar, game.input.activePointer) > 8) {
+                    //  Make the object seek to the active pointer (mouse or touch).
+                    game.physics.arcade.moveToPointer(mycar, 300);
+                }
+                else {
+                    //  Otherwise turn off velocity because we're close enough to the pointer
+                    mycar.body.velocity.set(0);
+                }
 
-    catHead = game.add.group();
-    catHead.enableBody = true;
+                // After traffic passes mycar and goes down the road
+                // Move it back up the road to go by again and faster
+                if (dumptruck.body.x < mycar.body.x - 1000) {
+                    dumptruck.body.x = mycar.body.x + 1000;
+                    dumptruck.body.velocity.x -= 20;
+                }
+                // Make it veer toward your lane and get faster
+                dumptruck.body.y += 0.1;
+                if (dumptruck.body.y > game.world.centerY + 100) {
+                    dumptruck.body.y = game.world.centerY - 150;
+                }
 
-    for (var i = 0; i < 50; i++)
-    {
-        var s = catHead.create(game.world.randomX, game.world.randomY, 'baddie');
-        s.body.collideWorldBounds = true;
-        s.body.bounce.set(1);
-        s.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
-    }
+                game.physics.arcade.overlap(mycar, dumptruck, crash,null,this);
 
-    humanHead = game.add.sprite(400, 400, 'humanHead');
-    humanHead.scale.x = 0.5;
-    humanHead.scale.y = 0.5;
+                if(myswitch = true){
+                    game.debug.text('Game Over',32,62);
+                }
+                else{
+                    game.debug.text('Avoid the truck',32,62);
+                }
 
-    game.physics.enable(humanHead, Phaser.Physics.ARCADE);
+            }
 
-    humanHead.body.collideWorldBounds = true;
-    humanHead.body.bounce.set(1);
-
-    cursors = game.input.keyboard.createCursorKeys();*/
-
-}
-
-function update() {
-
-    /*game.physics.arcade.collide(humanHead, catHead);
-
-    if (cursors.left.isDown)
-    {
-        humanHead.body.velocity.x -= 4;
-    }
-    else if (cursors.right.isDown)
-    {
-        humanHead.body.velocity.x += 4;
-    }
-
-    if (cursors.up.isDown)
-    {
-        humanHead.body.velocity.y -= 4;
-    }
-    else if (cursors.down.isDown)
-    {
-        humanHead.body.velocity.y += 4;
-    }*/
-
-}
-};
+            function crash(mycar, dumptruck){
+                mycar.kill();
+                myswitch = true;
+            }
+                
+        };
